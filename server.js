703bf36ffed81d3398f383c1d46dce259c6d1c1e
Syed Asad Zaman <essayyzed@ip-192-168-10-5.me-south-1.compute.internal>
2023-12-29 22:42:18 +0500

    Added a simple route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).json({ success: false, "data": {"id": 1}})
+})
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(
