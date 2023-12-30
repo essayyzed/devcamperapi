2b85462633ccdd2fdc8e33ec2e1d8c66e71b92ba
Syed Asad Zaman <essayyzed@zenbook.local>
2023-12-30 22:02:36 +0500

    Add MongoDB connection using mongoose

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const connectDB = require('./config/db');
+
+//? Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//? Connect Database
+connectDB();
 
 //? Route Files
-
 const bootcamps = require('./routes/bootcamps');
-
-// Load env vars
-dotenv.config({ path: './config/config.env' });
+const morgan = require('morgan');
 
 const app = express();
 
-app.use(logger)
+//? Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// app.use(logger)
 
 //? Mount Routers
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT} }`,
-  ),
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT} }`
+  )
 );
+
+//? Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //! close server and exit process
+  server.close(() => process.exit(1));
+});
